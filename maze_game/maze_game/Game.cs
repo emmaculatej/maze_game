2efd470b6e9356b6ce144ccc50ddfb15c6dd2ea2
Emma Jeyarajah <ej00190@surrey.ac.uk>
2018-03-12 22:53:54 +0000

    Create Game Loop

---
+++ maze_game/maze_game/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maze_game
+{
+    class Game
+    {
+        public bool IsRunning { get; set; }
+
+        public void GameInitialisation()
+        {
+            IsRunning = true;
+        }
+
+        public void GameLoop()
+        {
+            while (IsRunning)
+            {
+
+            }
+        }
+    }
+}
